1d7e75459c79f3a6a4574f3f2385f8cc4c870705
dsh91698 <56885229+dsh91698@users.noreply.github.com>
2021-12-31 15:12:06 +0400

    init: task reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+let n_str = Math.abs(n).toString(); // take absolute value and transform number to string
+return [...n_str].reverse().join(''); // spread number intu array, reverse and join back
+
 }
